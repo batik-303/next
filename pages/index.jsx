b58bf3a588379a4b2fc59c185c13c78d42e5d281
Bahtiyor Sultanov <bahtiyor.sultanov@gtsol.com>
2022-04-10 21:41:43 +0200

    next js initialized and cleaned up

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='bg-gray-200 p-8 border-2'>
+        <h1>Hello world</h1>
+    </div>
+  )
+}
